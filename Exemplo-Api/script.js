f5ed623f129f68520a5de15175d369056c74c600
Emerson Felisberto <emersonfelisbertoemf@gmail.com>
2021-04-14 14:02:38 -0300

    Api de cep

--- Exemplo-Api/script.js
+++ Exemplo-Api/script.js
@@ -6,7 +6,7 @@ function acessarAPI(link,executarFuncao){
     xhtml.onreadystatechange = function(){
       if ( this.readyState == 4 &&
         this.status ==  200 ) {
-            console.log(link);
+          
             executarFuncao(this.responseText);
 
         }

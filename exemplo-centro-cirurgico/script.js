ecc5eaa4705e754f221ac2a143e964385e25351d
Emerson Felisberto <emersonfelisbertoemf@gmail.com>
2021-04-18 08:52:29 -0300

    Mudanças

--- exemplo-centro-cirurgico/script.js
+++ exemplo-centro-cirurgico/script.js
@@ -1,4 +1,4 @@
-lsPessoa = [];
+var lsPessoa = JSON.parse (localStorage.getItem("lsPessoa"));
 function gravar(){
     var pessoa={};
 
@@ -9,23 +9,33 @@ function gravar(){
     pessoa.inicioPrevisto= document.getElementById("inicioPrevisto").value;
     pessoa.fimPrevisto= document.getElementById("fimPrevisto").value;
     pessoa.saidaPrevisto= document.getElementById("saidaPrevisto").value;
+     var id= document.getElementById("id").value;
 
-console.log(pessoa);
-lsPessoa.push(pessoa);
+    if (id == ""){
+        lsPessoa.push(pessoa);
+    }else {
+        lsPessoa[id]=pessoa;
+    }
+
+
 
 localStorage.setItem("lsPessoa" , JSON.stringify(lsPessoa))
 uptadeTable()
 
+document.getElementById("formulario").reset();
+
+
 }
 function uptadeTable(){
     console.log(uptadeTable)
+
 var lsPessoa = JSON.parse (localStorage.getItem("lsPessoa"));
 var bodyTable = "";
 for( i in lsPessoa){
-    bodyTable += `<tr>`;
+    bodyTable += `<tr onclick="editar(${i})"> `;
     bodyTable += `<td> ${lsPessoa[i].nome}</td>`;
     bodyTable += `<td> ${lsPessoa[i].status}</td>`;
-    bodyTable += `<td> ${lsPessoa[i].local}</td>`;
+   // bodyTable += `<td> ${lsPessoa[i].local}</td>`;
     bodyTable += `<td> ${lsPessoa[i].horaInicio}</td>`;
     bodyTable += `<td> ${lsPessoa[i].inicioPrevisto}</td>`;
     bodyTable += `<td> ${lsPessoa[i].fimPrevisto}</td>`;
@@ -34,6 +44,28 @@ for( i in lsPessoa){
    
 }
 document.getElementById("bodyTable").innerHTML = bodyTable;
+
+}
+
+function editar(id){
+
+    document.getElementById('nome').value=lsPessoa[id].nome ;
+    document.getElementById("status").value=lsPessoa[id].status ;
+    document.getElementById("local").value=lsPessoa[id].local ;
+    document.getElementById("horaInicio").value=lsPessoa[id].horaInicio ;
+    document.getElementById("inicioPrevisto").value=lsPessoa[id].inicioPrevisto ;
+    document.getElementById("fimPrevisto").value=lsPessoa[id].fimPrevisto;
+    document.getElementById("saidaPrevisto").value=lsPessoa[id].saidaPrevisto;
+    document.getElementById("id").value=id; 
+
+
+
+}
+
+function apagar (){
+    var id= document.getElementById("id").value;
+
+    lsPessoa.splice(id,1);
 
 }
 uptadeTable()

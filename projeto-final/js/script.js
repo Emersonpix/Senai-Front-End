258f8b93888b3c138af496ac59ec80a76dcd2d7d
Emerson Felisberto <emersonfelisbertoemf@gmail.com>
2021-04-28 18:09:59 -0300

    Ajustes Projeto

--- projeto-final/js/script.js
+++ projeto-final/js/script.js
@@ -16,4 +16,16 @@ function atualizarParcial(){
 resultado = `R$  ${(9.0 * qtProduto).toFixed(1) }   (x${qtProduto}) `
 document.getElementById("parcial").innerHTML = resultado;
 }
+ 
+var acc = document.getElementsByClassName("conteudo");
+acc[0].addEventListener("click" , function(){
+ 
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+})
 
